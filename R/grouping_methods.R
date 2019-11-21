71821c37bec3ae3c088a2079bc0d51f78146f722
Antoni Baum <antoni.baum@gmail.com>
2019-11-21 12:50:20 +0100

    Add group_by_average function

---
+++ R/grouping_methods.R
@@ -0,0 +1,51 @@
+# Grouping methods.
+
+#' Group linearly ordered alternatives by average.
+#'
+#' @param ordered_df A data frame with elements \code{alt.row}, \code{score}
+#'   and \code{rank} (returned from any linear ordering method from this library).
+#' @return A list including elements:
+#' \describe{
+#'   \item{\code{highest}}
+#'   \item{\code{above_average}}
+#'   \item{\code{below_average}}
+#'   \item{\code{lowest}}
+#' }
+#' @author Antoni Baum \email{antoni.baum@protonmail.com}
+#' @examples
+#' d <- matrix(rpois(12, 5), nrow = 3, ncol = 3)
+#' w <- c(1, 1, 2)
+#' i <- c('+', '-', '+')
+#' group_by_average(sum_of_ranks(d, w, i))
+#' @export
+group_by_average <- function(ordered_df) {
+  mean_score = mean(ordered_df$score)
+  sd_score = sd(ordered_df$score)
+  groups <-
+    list(
+      "highest" = c(),
+      "above_average" = c(),
+      "below_average" = c(),
+      "lowest" = c()
+    )
+  for (i in 1:nrow(ordered_df))
+  {
+    if (ordered_df$score[i] >= mean_score + sd_score) {
+      groups$highest <- c(groups$highest, ordered_df$alt.row[i])
+    }
+    else if (mean_score <= ordered_df$score[i] &&
+        ordered_df$score[i] < mean_score + sd_score) {
+      groups$above_average <-
+        c(groups$above_average, ordered_df$alt.row[i])
+    }
+    else if (mean_score - sd_score <= ordered_df$score[i] &&
+        ordered_df$score[i] < mean_score) {
+      groups$below_average <-
+        c(groups$below_average, ordered_df$alt.row[i])
+    }
+    else {
+      groups$lowest <- c(groups$lowest, ordered_df$alt.row[i])
+    }
+  }
+  return (groups)
+}
